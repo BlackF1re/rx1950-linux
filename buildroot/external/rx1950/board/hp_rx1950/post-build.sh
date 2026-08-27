52d8f9f22951ddb77ed07d542c3da17c3bb2c3e0
BlackF1re <55582873+BlackF1re@users.noreply.github.com>
2026-08-27 20:37:24 +0700

    feat: install wlan runtime helpers

--- buildroot/external/rx1950/board/hp_rx1950/post-build.sh
+++ buildroot/external/rx1950/board/hp_rx1950/post-build.sh
@@ -11,5 +11,15 @@ install -d -m 0755 \
     "${TARGET_DIR}/tmp" \
     "${TARGET_DIR}/run" \
     "${TARGET_DIR}/mnt/data" \
+    "${TARGET_DIR}/mnt/rx1950-boot" \
+    "${TARGET_DIR}/lib/firmware" \
     "${TARGET_DIR}/var/lib/opkg/lists"
 install -m 0644 "${EXTERNAL_DIR}/board/hp_rx1950/fstab" "${TARGET_DIR}/etc/fstab"
+
+chmod 0755 \
+    "${TARGET_DIR}/etc/init.d/S10kernel-modules" \
+    "${TARGET_DIR}/etc/init.d/S40wlan" \
+    "${TARGET_DIR}/usr/sbin/rx1950-wlan" \
+    "${TARGET_DIR}/usr/sbin/rx1950-wlan-firmware" \
+    "${TARGET_DIR}/usr/sbin/rx1950-blue" \
+    "${TARGET_DIR}/usr/sbin/rx1950-sensors"
